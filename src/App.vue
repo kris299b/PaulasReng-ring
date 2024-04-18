e685e8615a1bd3fd17f1da2ad66a6931a3ca0209
kris299b <kripag01@easv365.dk>
2024-04-18 10:55:51 +0200

    code

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-stone-100 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+  </div>
+  <div class="block lg:hidden">
+    <RouterLink to="/">
+      <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </RouterLink>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-xs lg:flex-grow">
+      <ul class="flex flex-row space-x-6 mr-2 text-gray-500 text-xs md:text-sm md:mr-2 lg:mr-2 md:space-x-10 lg:space-x-12 lg:ml-10 lg:text-xs">
+        <li>
+          <RouterLink to="/">HJEM</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="guide">GUIDE</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="services">JEG TILBYDER</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="about">OM FIRMAET</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="prices">PRISER</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="kontakt">KONTAKT</RouterLink>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <button class="text-gray-500 bg-transparent hover:bg-transparent border-2 border-gray-300 font-semibold py-2 px-8 rounded-full lg:text-sm lg:text-gray-500">
+        BOOK EN TID
+      </button>
+    </div>
+  </div>
+</nav>
+<RouterView />
+
+
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="relative h-screen flex flex-col items-center py-20 text-center content-start text-white">
+        <div class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+            <video class="min-w-full min-h-full absolute object-cover"
+                src="../assets/herovideo.mp4"
+                type="video/mp4" autoplay muted loop>
+            </video>
+        </div>
+        <div class="video-content z-10 pt-20 lg:pt-32">
+            <p class="text-white text-1xl md:text-2xl lg:text-sm">PROFESSIONEL RENGØRING</p>
+            <h2 class="text-white text-3xl md:text-6xl lg:text-4xl py-1">MED DIG SOM KUNDE I CENTRUM</h2>
+            
+            <div class="flex flex-col text-sm pt-5 lg:flex-row lg:space-x-4 lg:justify-center lg:text-sm lg:pt-5">
+                <button class="bg-stone-500 hover:stone-500 text-white font-semibold py-2 px-8 rounded-full">
+                    JEG TILBYDER
+                </button>
+                <button class="bg-transparent hover:bg-transparent border-2 text-white font-semibold py-2 px-8 rounded-full">
+                    KONTAKT MIG
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style>
+
+</style>
+
+<script setup>
+
+</script>
