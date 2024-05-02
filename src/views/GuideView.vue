a581341aa8e6774e8dfad721967c50cfa6d228fd
kris299b <kripag01@easv365.dk>
2024-05-02 12:51:51 +0200

    Hero-section with image added to the Guide-page

--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -1,5 +1,17 @@
 <template>
- 
+<!--Hero-section with image-->
+<section>
+<div class="relative h-full w-full md:h-full md:w-full lg:h-full lg:w-full">
+    <img class="object-cover"src="../assets/hero-copi-copi.jpg" alt="Kvinde der støvsuger">
+
+    <div class="absolute inset-0 flex flex-col items-center justify-center">
+        <h1 class="text-white text-xs pb-2 pt-20 md:pb-4 md:text-xl lg:text-xl lg:pb-6 lg:pt-0">GUIDE</h1>
+        <p class="text-white text-base md:text-4xl md:pb-2 lg:text-5xl lg:pb-2">SÅDAN FÅR DU MEST UD AF DIN</p>
+        <p class="text-white text-base md:text-4xl md:pb-2 lg:text-5xl lg:pb-2">RENGØRING</p>
+    </div>
+</div>
+</section>
+
 </template>
 
 <style>
