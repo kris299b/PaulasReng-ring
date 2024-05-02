300728ffe1958f894944016676d95925927c45aa
kris299b <kripag01@easv365.dk>
2024-05-02 18:22:08 +0200

    Alterations in the Hero-section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,32 @@
 <!--Hero-section with image-->
 <section>
     <div class="relative h-full w-full md:h-full md:w-full lg:h-full lg:w-full">
-        <img class="object-cover" src="../assets/hero-copi-copi.jpg" alt="Kvinde som støvsuger">
-    </div>
+        <img class="object-cover"src="../assets/hero-copi-copi.jpg" alt="Kvinde der støvsuger">
 
     <div class="absolute inset-0 flex flex-col items-center justify-center">
-        <h1 class="text-white text-xs pb-2 pt-20 md:text-xl md:pb-6 lg:text-xl lg:pb-6">OM FIRMAET</h1>
-        <p class="text-white text-base md:text-4xl md:pb-2 lg:text-5xl ">PAULAS RENGØRING</p>
+        <h1 class="text-white text-xs pb-2 pt-20 md:pb-4 md:text-xl lg:text-xl lg:pb-6 lg:pt-0">OM FIRMAET</h1>
+        <p class="text-white text-base md:text-4xl md:pb-2 lg:text-5xl lg:pb-2">PAULAS RENGØRING</p>
     </div>
+</div>
 </section>
+
+
+<!--Value-section-->
+<div class="bg-blue dark:bg-white light:bg-white flex flex-col ">
+    <div class="">
+        <h2 class="text-black">MINE VÆRDIER</h2>
+        <h1 class="text-black">TILLID</h1>
+        <p class="text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae quo sapiente animi veniam impedit ratione quidem, neque nesciunt voluptatibus, illum dolore illo? Aspernatur accusamus minima suscipit maiores, voluptates reiciendis voluptate!</p>
+        <h1 class="text-black">KUNDETILFREDSHED</h1>
+        <p class="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima illum veniam et necessitatibus eligendi fugit facilis? Minus ratione cupiditate nulla, accusamus tenetur est odit molestias culpa error corrupti! Aliquid, enim.</p>
+        <h1 class="text-black">TÆT SAMARBEJDE</h1>
+        <p class="text-black">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus quis, eos iusto beatae veritatis natus, suscipit cupiditate, saepe explicabo pariatur itaque debitis quod vitae. Veritatis dignissimos assumenda sed tempore corrupti!</p>
+    </div>
+
+    <div class="hidden lg:block lg:w-1/3">
+        <img src="../assets/PaulasRengøring-værdibillede.jpg" alt="Kvinde der smiler">
+    </div>
+</div>
 </template>
 
 <style>
