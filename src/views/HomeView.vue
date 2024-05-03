fd87c61c581f9369bddb5128269a5573c68b84b4
kris299b <kripag01@easv365.dk>
2024-05-03 14:00:00 +0200

    added contactform to homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -224,53 +224,60 @@
 </div>
 
 <!-- kontaktformular sektion -->
-<section class="relative h-screen flex flex-col items-center text-center content-start pt-10 py-12 lg:py-12">
+<section class="relative h-full flex flex-col items-center text-center content-start pt-10 py-12 lg:py-12">
     <div class="absolute top-0 left-0 w-full h-full overflow-hidden">
         <img class="min-w-full min-h-full absolute object-cover" src="../assets/Sprøgsmål-billede.jpg" alt="Private-cleaning">
     </div>
-    <div class="image-content text-white dark:text-white z-10 pt-0 md:pt-10 lg:pt-0">
-        <p class="pt-8 text-sm md:mx-5 md:pt-2 lg:text-sm lg:font-semibold lg:pt-0 text-white dark:text-white lg:text-center lg:tracking-wider">PAULAS RENGØRING | KONTAKT</p>
+    <div class="image-content text-white dark:text-white z-10 md:pt-10">
+        <p class="text-sm md:mx-5 lg:text-sm lg:font-semibold lg:pt-0 text-white dark:text-white lg:text-center lg:tracking-wider">PAULAS RENGØRING | KONTAKT</p>
         <h2 class="font-sans text-4xl font-semibold text-center md:text-center lg:text-5xl lg:font-semibold lg:text-center">
             Har du spørgsmål?
         </h2>
-        <p class="font-sans text-sm text-center px-10 pt-3 md:pt-4 md:text-center md:px-10 lg:text-center lg:text-sm lg:px-40 lg:pt-5">There are many variations of passages of Lorem Ipsum available, but the majority 
+        <p class="pb-10 font-sans text-sm text-center px-10 pt-3 md:pt-4 md:text-center md:px-10 lg:text-center lg:text-sm lg:px-40 lg:pt-5">There are many variations of passages of Lorem Ipsum available, but the majority 
             have suffered alteration in some form. by injected humour, or randomised words which
             don't look even slightly believable. 
         </p>
     </div>
-    <div class="flex justify-center lg:space-x-0 lg:pt-10 lg:w-4/6 z-20 lg:pt-24">
-        <div class="bg-white dark:bg-white max-w-lg shadow-sm">
+    
+    <div class="flex justify justify-center flex-col lg:flex-row lg:space-x-0 lg:pt-10 lg:w-3/4 z-20 lg:pt-24">
+        <div class="bg-white dark:bg-white max-w-lg shadow-sm lg:w-2/3">
             <div class="bg-white dark:bg-white max-w-lg">
-            <div class="py-8 px-4 mx-auto max-w-screen-md lg:pt-0">
-                <h2 class="mb-4 text-4xl tracking-tight font-semibold text-center text-black dark:text-black lg:text-3xl lg:px-5 lg:pr-10 lg:font-semibold lg:pt-10 lg:text-left">
+                <h2 class="mb-4 text-4xl tracking-tight font-semibold text-left pt-10 px-5 text-black dark:text-black md:px-5 md:pt-10 md:text-3xl md:text-left lg:text-3xl lg:px-5 lg:pr-10 lg:font-semibold lg:pt-10 lg:text-left">
                     Lad os tage en uforpligtende snak om dine ønsker og behov.
                 </h2>
-                <p class="mb-8 lg:mb-16 text-xl font-light text-center text-black dark:text-black lg:mb-4 lg:px-5 lg:font-normal lg:text-sm lg:text-left">
+                <p class="mb-8 text-sm text-left px-5 font-light text-black dark:text-black md:px-5 md:text-sm md:text-left lg:mb-16 lg:mb-4 lg:px-5 lg:font-normal lg:text-sm lg:text-left">
                     Udfyld venligst formularen herunder, så kontakter jeg dig hurtigst muligt.
                 </p>
-                <form action="#" class="space-y-8 lg:text-left lg:px-5 lg:space-y-6">
-                    <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-black dark:text-black">Din email</label>
-                        <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required>
+                <!--Mailchimp Contactform-->
+                <div id="mc_embed_shell">
+                    <div id="mc_embed_signup">
+                        <form action="https://gmail.us17.list-manage.com/subscribe/post?u=529fa2bc5942a0147307ea268&amp;id=90a02a22b5&amp;v_id=4389&amp;f_id=00f8d5e3f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                            <div id="mc_embed_signup_scroll">
+                                <div class="mc-field-group text-black"><label for="mce-EMAIL">Din email <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div><div class="mc-field-group text-black font-semibold"><label for="mce-FNAME">Dit fulde navn <span class="asterisk">*</span></label><input type="text" name="FNAME" class="required text" id="mce-FNAME" required="" value=""></div><div class="mc-field-group text-black font-semibold"><label for="mce-MMERGE3">Din besked </label><input type="text" name="MMERGE3" class=" text" id="mce-MMERGE3" value=""></div><div class="indicates-required text-black font-semibold text-sm"><span class="asterisk">*</span>Påkrævet</div><div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group"><div class="content__gdpr text-black font-bold text-base"><label>GDPR - overholdelseserklæring</label></div><div class="content__gdpr text-black text-sm"><p>Vi tager dit privatliv alvorligt. Ved at indsende denne formular giver du dit samtykke til indsamling, behandling og opbevaring af dine personlige data i henhold til vores privatlivspolitik. Dine oplysninger vil udelukkende blive brugt med det formål at besvare din henvendelse. </p><div class="content__gdpr text-black font-semibold text-xs"></div><fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field"><label class="checkbox subfield" for="gdpr212271"><input type="checkbox" id="gdpr_212271" name="gdpr[212271]" class="gdpr" value="Y"><span>Ja</span></label><label class="checkbox subfield" for="gdpr212274"><input type="checkbox" id="gdpr_212274" name="gdpr[212274]" class="gdpr" value="Y"><span>Nej</span></label></fieldset><p class="text-base pt-4">For mere information om håndtering dine data og rettigheder under GDPR, henvises til vores privatlivspolitik her.</p></div><div class="content__gdprLegal text-black"><p class="pt-4 pb-4">Vi bruger Mailchimp til vores kontaktforme.<a class="text-darkblue" href="https://mailchimp.com/legal/terms"> Læs mere</a> om Mailchimp's privatlivspolitik.</p></div></div>
+                                <div id="mce-responses" class="clear foot">
+                                    <div class="response" id="mce-error-response" style="display: none;"></div>
+                                    <div class="response" id="mce-success-response" style="display: none;"></div>
+                                </div>
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true">/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+                                    <input type="text" name="b_529fa2bc5942a0147307ea268_90a02a22b5" tabindex="-1" value="">
+                                </div>
+                                <div class="optionalParent">
+                                    <div class="clear foot">
+                                        <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="SEND">
+                                        <p style="margin: 0px auto;"><a href="http://eepurl.com/iPjQYc" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"></span></a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                        <label for="subject" class="block mb-2 text-sm font-medium text-black dark:text-black">Navn</label>
-                        <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Dit navn" required>
-                    </div>
-                    <div class="sm:col-span-2">
-                        <label for="message" class="block mb-2 text-sm font-medium text-black dark:text-black">Din besked</label>
-                        <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Hvad kan jeg hjælpe med?"></textarea>
-                    </div>
-                    <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-full sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 bg:darkbeige dark:bg-darkbeige dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send besked</button>
-                </form>
+                </div>
             </div>
         </div>
-        </div>
-        <div class="bg-lightgray light:bg-lightgray dark:bg-lightgray max-w-lg lg:px-5">
-            <h2 class="font-sans text-2xl text-black pl-5 pr-10 pt-10 font-semibold md:pb-10 md:font-semibold md:text-center lg:pt-10 lg:pb-0 lg:text-black lg:text-2xl lg:font-semibold lg:text-left">
+        <div class="h-48 bg-lightgray light:bg-lightgray dark:bg-lightgray md:h-60 lg:h-96 lg:w-1/3 lg:justify-start lg:text-left">
+            <h2 class="justify-start pr-24 font-sans text-2xl text-black pt-6 pb-6 font-semibold md:pr-60 md:pb-10 md:font-semibold md:text-center lg:pt-24 lg:pb-0 lg:text-black lg:text-2xl lg:font-semibold lg:pl-8">
                 Kontaktoplysninger 
             </h2>
-            <div class="flex lg:pt-10 lg:pl-5">
+            <div class="flex justify-start px-8 pb-2 lg:pt-10">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#444444" class="w-4 h-4">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
@@ -280,7 +287,7 @@
                     60145811
                 </p>
             </div>
-            <div class="flex lg:pt-5 lg:pl-5">
+            <div class="flex justify-start px-8 py-2 lg:pt-5">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0,0,256,256" style="fill:#000000;">
                         <g fill="#444444" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
@@ -294,7 +301,7 @@
                     PAULAS RENGØRING
                 </p>
             </div>
-            <div class="flex lg:pt-5 lg:pl-5 lg:pb-10">
+            <div class="flex justify-start px-8 py-2 lg:pt-5">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="#444444">
                         <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.89 2 1.99 2H20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
@@ -311,9 +318,9 @@
 
 
 <!-- Kundeanmeldelser -->
-<div class="bg-white light:bg-white dark:bg-white lg:pt-40">
+<div class="bg-white light:bg-white dark:bg-white lg:pt-10">
     <div>
-        <h2 class="font-sans text-black text-2xl font-semibold dark:text-black pt-10 pb-10 text-center lg:text-4xl lg:text-center lg:font-semibold lg:pt-64 lg:pb-20">
+        <h2 class="font-sans text-black text-2xl font-semibold dark:text-black pt-10 pb-10 text-center lg:text-4xl lg:text-center lg:font-semibold lg:pb-20">
             Det siger mine kunder 
         </h2>
     </div>
@@ -428,14 +435,14 @@
     </div>
 </section>
 
-
-
-
-
 </template>
 
 <style>
-
+#mc-embedded-subscribe-form input[type=checkbox]{display: inline; width: auto;margin-right: 10px;}
+#mergeRow-gdpr {margin-top: 20px;}
+#mergeRow-gdpr fieldset label {font-weight: normal;}
+#mc-embedded-subscribe-form .mc_fieldset{border:none;min-height: 0px;padding-bottom:0px;}
+#mc-embedded-subscribe {background-color:rgb(110, 118, 167); border-radius: 50%;}
 </style>
 
 <script setup>
