ff3d0528dace731be66b19e20da934b4403b8ee8
kris299b <kripag01@easv365.dk>
2024-05-15 00:24:34 +0200

    Responsiveness-alterations on all screen sizes

--- src/views/PricesView.vue
+++ src/views/PricesView.vue
@@ -30,27 +30,27 @@
 </div>
 
 <!-- Om sektion -->
-<div class="bg-white dark:bg-white light:bg-white flex justify justify-center h-20 pt-4 md:pt-10">
-    <h2 class="font-sans text-2xl text-black pt-0 pb-2 font-semibold md:pb-0 md:font-semibold md:text-3xl md:px-20 md:text-center lg:pt-0 lg:pb-5 lg:text-black lg:text-3xl lg:font-semibold lg:text-center">Dit Lokale rengøringsfirma</h2>
+<div class="bg-white dark:bg-white light:bg-white flex justify justify-center h-10 md:pt-10 lg:h-24">
+    <h2 class="font-sans text-xl text-black font-semibold pt-6 md:pb-0 md:font-semibold md:text-3xl md:px-20 md:text-center lg:text-black lg:text-3xl lg:font-semibold lg:text-center">Dit Lokale rengøringsfirma</h2>
 </div>
 
 <div class="bg-white dark:bg-white light:bg-white flex flex-col justify justify-center h-1/2 w-full p-8 pb-20 md:pb-20 md:flex-row lg:flex-row lg:w-full lg:pb-16">
     <div class="bg-lightgray p-4 pt-2 md:p-6 md:pb-0 md:w-1/2 lg:w-2/5 lg:px-12">
         <p class="text-gray dark:gray text-xs pt-5 font-semibold pb-2 md:pt-2 lg:pt-8 lg:text-sm lg:tracking-wider">PAULAS RENGØRING | OM MIG</p>
-        <h2 class="text-black dark:text-black text-2xl font-semibold pb-4 lg:pb-6 lg:text-3xl">Jeg værdsætter tillid og tryghed, i det samarbejde jeg indgår med mine kunder </h2>
+        <h2 class="text-black dark:text-black text-xl font-semibold pb-4 lg:pb-6 lg:text-3xl">Jeg værdsætter tillid og tryghed, i det samarbejde jeg indgår med mine kunder </h2>
         <p class="text-black dark:text-black text-sm lg:text-base">Personligt betyder by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need. </p>
         <p class="text-black dark:text-black text-sm lg:text-base lg:pt-5">There are many variations of passages of Lorem Ipsum available, but the majority. So if you know many variations of passages of Lorem Ipsum. </p>
-        <div class="row pt-6 pb-4 md:flex lg:flex lg:pt-8 lg:pb-0">
+        <div class="row pt-4 pb-4 md:flex lg:flex lg:pt-8 lg:pb-0">
             <div class="py-3 md:py-0 md:pr-3 lg:pt-0 lg:pr-3">
                 <RouterLink to="privatecleaning"> 
-                    <button class="bg-blue dark:bg-blue hover:bg-darkblue text-lightgray dark:text-lightgray font-semibold py-2 px-8 md:py-1 md:px-4 md:py-1 md:px-6 lg:text-md lg:py-2 lg:px-8 rounded-full lg:text-md">
+                    <button class="bg-blue dark:bg-blue hover:bg-darkblue text-lightgray dark:text-lightgray font-semibold py-3 px-6 md:py-2 md:px-4 lg:text-md lg:py-2 lg:px-6 rounded-full lg:text-md">
                         Privatrengøring
                     </button>
                 </RouterLink> 
             </div>
             <div class="lg:pt-0">
                 <RouterLink to="commercialcleaning"> 
-                    <button class="bg-blue dark:bg-blue hover:bg-darkblue text-lightgray dark:text-lightgray font-semibold py-2 px-8 md:py-1 md:px-4 md:py-1 md:px-6 lg:text-md lg:py-2 lg:px-8 rounded-full lg:text-md">
+                    <button class="bg-blue dark:bg-blue hover:bg-darkblue text-lightgray dark:text-lightgray font-semibold py-3 px-6 md:py-2 md:px-4 lg:text-md lg:py-2 lg:px-6 rounded-full lg:text-md">
                         Erhvervsrengøring
                     </button>
                 </RouterLink>
