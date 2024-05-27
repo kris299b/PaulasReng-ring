f5fbbf351f163c3832f3f56785e6132da0389581
kris299b <kripag01@easv365.dk>
2024-05-27 15:45:41 +0200

    Checking of the responsive screen sizes

--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -6,12 +6,12 @@
         <img class="min-w-full h-[50vh] md:h-[40vh] lg:h-[80vh] absolute object-cover" src="../assets/Guide-hero.jpg" alt="Kvinde rengør overflade i køkken">
         <div class="overlay absolute top-0 left-0 w-full h-full bg-black dark:bg-black opacity-40"></div>
     </div>
-    <div class="image-content z-10 pt-28 md:pt-40 lg:pt-48 lg:px-40">
+    <div class="image-content z-10 pt-28 md:pt-40 md:px-10 lg:pt-48 lg:px-40">
         <div>
-            <p class="font-sans text-white text-left pl-5 dark:text-white px-4 text-sm md:text-lg md:text-center lg:text-lg lg:text-left lg:tracking-wider">
+            <p class="font-sans text-white text-left pl-5 dark:text-white px-4 text-sm md:text-lg md:text-left lg:text-lg lg:text-left lg:tracking-wider">
                 PAULAS RENGØRING | VÆRD AT VIDE
             </p>
-            <h2 class="font-sans text-white dark:text-white text-left font-semibold px-4 text-4xl md:text-5xl md:text-center lg:pb-2 lg:font-semibold lg:text-5xl lg:text-left">Få mest ud af din professionelle rengøringshjælp</h2>
+            <h2 class="font-sans text-white dark:text-white text-left font-semibold px-4 text-4xl md:text-5xl md:text-left lg:pb-2 lg:font-semibold lg:text-5xl lg:text-left">Få mest ud af din professionelle rengøringshjælp</h2>
         </div>
     </div>
 </section>
@@ -44,7 +44,7 @@
 
 <!--Box 2-->
 <div class="flex bg-white dark:bg-white light:bg-white flex flex-col justify justify-center w-full pt-20 pb-8 md:flex-row md:justify-end lg:flex-row lg:w-full lg:pb-10 lg:mx-auto lg:justify-start">
-    <div class="w-full p-4 md:w-2/4 lg:w-2/4 md:pt-10 md:pl-8 lg:pl-8 lg:pt-16 lg:pr-8 bg-lightgray">
+    <div class="w-full p-4 md:w-2/4 lg:w-2/4 md:pt-10 md:pl-8 lg:pl-8 lg:pt-20 lg:pr-8 bg-lightgray">
         <h1 class="text-black font-semibold text-2xl pb-2 md:text-2xl lg:text-2xl md:pt-12 lg:pt-0">Udarbejd en arbejdsplan</h1>
         <p class="text-black text-sm font-semibold md:text-base md:pr-5 md:pb-3 md:font-semibold lg:font-semibold">Det vil være fordelagtigt at udarbejde en plan over dine ønsker og behov, som du deler med din rengøringshjælp i den indledende fase.</p>
         <p class="text-black text-sm pt-2 md:text-base md:pr-5 lg:pt-2 lg:text-base">Det er ønsker og krav I sammen vil gennemgå, og få til at passe ind i dine behov. Det er rart for begge parter, at der er en plan at holde sig til. Det vil selvfølgelig være en plan der kan ændre sig, alt efter dine ønsker. Det er dine behov og ønsker, der er i centrum.</p>
@@ -56,7 +56,7 @@
 </div>
 
 <!--Box 3-->
-<div class="flex bg-white dark:bg-white light:bg-white flex flex-col justify justify-center w-full pt-20 md:pt-10 md:flex-row md:justify-end lg:flex-row lg:w-full lg:pb-0 lg:mx-auto lg:justify-end">
+<div class="flex bg-white dark:bg-white light:bg-white flex flex-col justify justify-center w-full pt-16 md:pt-10 md:flex-row md:justify-end lg:flex-row lg:w-full lg:pb-0 lg:mx-auto lg:justify-end">
     <div class="md:w-2/4 lg:w-1/4">
         <img src="../assets/Oprydning-billede.jpg" alt="Legetøj flyder">
     </div>
@@ -85,9 +85,9 @@
 
 <!-- Servicefradrag-section -->
 <div class="bg:white light:bg-white dark:bg-white pt-10 pb-10 md:pt-20 lg:pb-20">
-    <div class="bg:lightgray dark:bg-lightgray font-sans lg:text-center lg:pt-10 lg:pb-10 lg:text-3xl text-black dark:text-black">
+    <div class="bg:lightgray dark:bg-lightgray font-sans text-black dark:text-black md:px-6 lg:text-center lg:pt-10 lg:pb-10 lg:px-16 lg:text-3xl">
         <h2 class="text-left font-semibold pl-5 px-10 text-2xl pt-10 md:text-center lg:text-center lg:text-3xl lg:font-semibold">Husk at din rengøringshjælp går ind under ''Servicefradrag''</h2>
-        <p class="text-left pl-5 pr-5 pt-3 pb-10 md:text-center lg:text-center lg:pl-40 lg:pr-40 lg:pt-4 lg:text-base">
+        <p class="text-left pl-5 pr-5 pt-3 pb-10 md:text-center lg:text-center lg:pt-4 lg:text-base">
             Når du køber privat rengøring, skal du huske at gemme alle dine kvitteringer. 
             Du kan nemlig trække beløb fra på din selvangivelse under rubrikken 
             ”servicefradrag”, det varierer en smule beløbsmæssigt fra indkomstår 
@@ -102,14 +102,14 @@
     <h2 class="font-sans text-2xl px-5 text-black font-semibold text-center md:font-semibold md:text-3xl md:px-20 md:text-center lg:pt-0 lg:text-black lg:text-3xl lg:font-semibold lg:text-center">Kunne du tænke dig at danne et samarbejde med mig?</h2>
 </div>
 
-<div class="bg-white dark:bg-white light:bg-white pt-6 pb-20 flex flex-col justify justify-center h-1/2 w-full p-8 pt-0 md:items-center md:p-0 md:pt-10 md:pb-20 md:flex-col lg:flex-row lg:w-full lg:pt-0 lg:pb-20">
-    <div class="bg-lightgray p-4 pt-0 md:p-6 md:pb-0 md:w-3/5 lg:w-2/5 lg:px-12">
-        <p class="text-gray dark:gray text-sm tracking-wider pt-8 font-semibold pb-1 md:pt-2 lg:pt-10 lg:text-sm lg:tracking-wider">OM MIG</p>
+<div class="bg-white dark:bg-white light:bg-white pt-6 pb-20 flex flex-col justify justify-center w-full p-8 pt-0 md:items-center md:p-0 md:pt-10 md:pb-20 md:flex-col lg:flex-row lg:w-full lg:pb-20">
+    <div class="bg-lightgray p-4 pt-0 md:p-6 md:pb-0 md:w-3/5 lg:w-2/3 lg:px-12">
+        <p class="text-gray dark:gray text-sm tracking-wider pt-8 font-semibold pb-1 md:pt-2 lg:text-sm lg:pt-12 lg:tracking-wider">OM MIG</p>
         <h2 class="text-black dark:text-black text-2xl font-semibold pb-4 md:pb-3 lg:pb-6 lg:text-3xl">Jeg værdsætter tillid og tryghed, i det samarbejde jeg indgår med mine kunder </h2>
         <p class="text-black dark:text-black text-base font-semibold md:pb-2 lg:font-semibold lg:text-base">Jeg startede Paulas Rengøring i 2014, da det længe havde været et ønske at blive selvstændig.</p>
         <p class="text-black dark:text-black text-base pt-3 md:pt-1 lg:text-base lg:pt-2 lg:text-base">Sidenhen har jeg opbygget en loyal kundekreds i Ringkøbing-Skjern kommune, som jeg værner om og værdsætter. Personligt betyder gensidig tillid, og et trygt samarbejde meget i de kunderelationer jeg indgår i. Jeg tilbyder rengøring for private samt erhverv.</p>
-        <div class="row pt-6 pb-4 md:flex lg:flex md:pt-3 lg:pt-6 lg:pb-0">
-            <div class="py-3 md:pt-5 md:pr-3 lg:pt-0 lg:pr-3">
+        <div class="row pt-2 pb-4 md:flex lg:flex md:pt-3 lg:pt-8 lg:pb-10">
+            <div class="py-6 md:pt-5 md:pr-3 lg:pt-0 lg:pr-3">
                 <RouterLink to="privatecleaning"> 
                     <button class="bg-blue dark:bg-blue hover:bg-darkblue text-lightgray dark:text-lightgray font-semibold py-2 px-8 md:py-2 md:px-3 md:py-1 md:px-6 lg:text-md lg:py-2 lg:px-8 rounded-full lg:text-md">
                         Privatrengøring
@@ -125,7 +125,7 @@
             </div>
         </div>
     </div>
-    <div class="md:w-3/5 lg:w-2/5">
+    <div class="md:w-3/5 lg:w-1/3">
         <img src="../assets/Paulasrengøring-om-billede.jpg" alt="Kvinde står med rengøringsprodukt">
     </div>
 </div>
